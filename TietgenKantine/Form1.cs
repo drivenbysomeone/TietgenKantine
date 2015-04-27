350b2c3921d706014360c6557c3650a05b63e8f0
drivenbysomeone <aspit1020@ASPITPC16.tietgen.dk>
2015-04-27 11:49:56 +0200

    changes in Sql and Forms

--- TietgenKantine/Form1.cs
+++ TietgenKantine/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace TietgenKantine
 {
@@ -15,6 +17,42 @@ namespace TietgenKantine
         public Form1()
         {
             InitializeComponent();
+            
+         
+            var connectionString = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+
+            string queryString = "SELECT * FROM MainCourse;";
+            DataTable dt = GetData(connectionString, queryString);
+            var list = new List<MainCourse>();
+            foreach (DataRow item in dt.Rows)
+            {
+                 var main = new MainCourse();
+                 main.Id = Convert.ToInt32(item["Id"].ToString());
+                 main.Name = item["MainCourseName"].ToString();
+                 list.Add(main);
+            }
+           
+
+            cmbDishes.ValueMember = "Id";
+            cmbDishes.DisplayMember = "Name";
+            cmbDishes.DataSource = list;
+        }
+
+        private static DataTable GetData(string connectionString, string queryString)
+        {
+             DataTable dt = new DataTable();
+            using (var connection = new SqlConnection(connectionString))
+            {
+               
+                var command = new SqlCommand(queryString, connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {           
+                        dt.Load(reader);
+                }
+            }
+            return dt;
+
         }
     }
 }
--- TietgenKantine/Properties/Settings.Designer.cs
+++ TietgenKantine/Properties/Settings.Designer.cs
@@ -8,22 +8,31 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TietgenKantine.Properties
-{
-
-
+namespace TietgenKantine.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=\\\\nrh1\\users$\\aspit1020\\dokumenter\\s" +
+            "qlFileKantine.mdf;Integrated Security=True;Connect Timeout=30")]
+        public string DB {
+            get {
+                return ((string)(this["DB"]));
+            }
+            set {
+                this["DB"] = value;
             }
         }
     }
